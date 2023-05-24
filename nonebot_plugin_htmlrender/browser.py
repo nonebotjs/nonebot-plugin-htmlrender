7caaa116f2cd02a64bcdba61bc9b859aeab39abe
Shigma <shigma10826@gmail.com>
2023-05-24 23:31:53 +0800

    add missing api

--- nonebot_plugin_htmlrender/browser.py
+++ nonebot_plugin_htmlrender/browser.py
@@ -15,11 +15,22 @@ class get_new_page():
   def __init__(self, viewport={}):
     pass
 
-  async def set_content(self, html):
-    await self.page.setContent(html)
+  async def goto(self, url: str):
+    if url.startswith('file://'):
+      url = 'file://' + ctx.nonebot.resolvePath(url[7:])
+    await self.page.goto(url)
 
-  async def screenshot(self, full_page=False):
+  async def wait_for_timeout(self, wait: int):
+    await self.page.waitForTimeout(wait)
+
+  async def set_content(self, html, wait_until=None):
+    await self.page.setContent(html, waitUntil=None if wait_until is None else 'networkidle0')
+
+  async def screenshot(self, type='png', full_page=False, quality=None):
     base64 = await self.page.screenshot(to_js({
+      'type': type,
       'encoding': 'base64',
+      'fullPage': full_page,
+      'quality': quality,
     }, dict_converter=Object.fromEntries))
     return "data:image/png;base64," + base64
